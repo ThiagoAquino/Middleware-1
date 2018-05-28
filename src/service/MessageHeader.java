3ad82eddf112a26646ad95da77410e3e55accfd3
thiagoaquino <thiagoaquiino@gmail.com>
2018-05-28 13:48:42 -0300

    Message.MessageHeader

---
+++ src/service/MessageHeader.java
@@ -0,0 +1,24 @@
+package service;
+
+import java.io.Serializable;
+
+public class MessageHeader implements Serializable {
+		private String destination;
+
+		public MessageHeader(String destination) {
+			this.destination = destination;
+		}
+
+		public String getDestination() {
+			return destination;
+		}
+
+		public void setDestination(String destination) {
+			this.destination = destination;
+		}
+		
+		
+	}
+
+	
+}
