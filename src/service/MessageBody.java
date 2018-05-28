aa9d0632f4ad916869a71be1b26832e53f1c232c
thiagoaquino <thiagoaquiino@gmail.com>
2018-05-28 13:50:58 -0300

    Message.MessageBody

---
+++ src/service/MessageBody.java
@@ -0,0 +1,20 @@
+package service;
+
+import java.io.Serializable;
+
+public class MessageBody implements Serializable{
+	private String content;
+
+	public MessageBody(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
--- src/service/MessageHeader.java
+++ src/service/MessageHeader.java
@@ -3,22 +3,20 @@ package service;
 import java.io.Serializable;
 
 public class MessageHeader implements Serializable {
-		private String destination;
+	private String destination;
 
-		public MessageHeader(String destination) {
-			this.destination = destination;
-		}
-
-		public String getDestination() {
-			return destination;
-		}
-
-		public void setDestination(String destination) {
-			this.destination = destination;
-		}
-		
-		
+	public MessageHeader(String destination) {
+		this.destination = destination;
 	}
 
-	
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+
 }
+
