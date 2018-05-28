6cd3209f2fddc40bd6f57e898c544dcf6dd3058e
thiagoaquino <thiagoaquiino@gmail.com>
2018-05-28 13:34:59 -0300

    Marshaller

---
+++ src/service/Marshaller.java
@@ -0,0 +1,26 @@
+package service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import middleware.message.Message2;
+
+public class Marshaller {
+	public static byte[] marshall(Message message) throws IOException {
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
+		objectStream.writeObject(message);
+
+		return byteStream.toByteArray();
+	}
+
+	public static Message unmarshall(byte[] message) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream byteStream = new ByteArrayInputStream(message);
+		ObjectInputStream objectStream = new ObjectInputStream(byteStream);
+
+		return (Message) objectStream.readObject();
+	}
+}
