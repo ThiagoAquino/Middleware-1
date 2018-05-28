8325148f6f085cf2ac95522f3f6a4169d0e67a48
thiagoaquino <thiagoaquiino@gmail.com>
2018-05-28 13:45:40 -0300

    Message

---
+++ src/service/Message.java
@@ -0,0 +1,31 @@
+package service;
+
+import java.io.Serializable;
+
+public class Message implements Serializable{
+	private MessageHeader header;
+	private MessageBody body;
+	
+	public Message(MessageHeader header, MessageBody body) {
+		this.header = header;
+		this.body= body;
+	}
+
+	public MessageHeader getHeader() {
+		return header;
+	}
+
+	public void setHeader(MessageHeader header) {
+		this.header = header;
+	}
+
+	public MessageBody getBody() {
+		return body;
+	}
+
+	public void setBody(MessageBody body) {
+		this.body = body;
+	}
+	
+	
+}
