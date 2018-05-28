2fe5adfa8faea0980b0aa10543d9a40f67112d98
thiagoaquino <thiagoaquiino@gmail.com>
2018-05-28 14:54:30 -0300

    Encryption

---
+++ src/service/Encryption.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.io.Serializable;
+
+public class Encryption implements Serializable  {
+	
+	public Encryption() {}
+	
+	//Falta definir qual será o modelo
+	public Message encrypt (Message entrada) {
+		Message retorno = null;
+		return retorno;
+	}
+	
+	public Message decrypt(Message entrada) {
+		Message retorno = null;
+		return retorno;
+	}
+}
--- src/service/Marshaller.java
+++ src/service/Marshaller.java
@@ -7,18 +7,25 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Marshaller {
+	static Encryption encry = new Encryption();
 	public static byte[] marshall(Message message) throws IOException {
 		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 		ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
-		objectStream.writeObject(message);
 
+		//Criptografando
+		Message msg = encry.encrypt(message);
+		
+		objectStream.writeObject(msg);		
 		return byteStream.toByteArray();
 	}
 
 	public static Message unmarshall(byte[] message) throws IOException, ClassNotFoundException {
 		ByteArrayInputStream byteStream = new ByteArrayInputStream(message);
 		ObjectInputStream objectStream = new ObjectInputStream(byteStream);
-
-		return (Message) objectStream.readObject();
+		
+		//Descriptografando
+		Message msg = encry.decrypt((Message) objectStream.readObject());
+		
+		return msg;
 	}
 }
