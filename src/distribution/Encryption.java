3f888c5c73ff6b4f4b459cba1596c11b9c9c44d1
Allan <ala6@cin.ufpe.br>
2018-06-11 17:08:50 -0300

    encryption second step

--- src/distribution/Encryption.java
+++ src/distribution/Encryption.java
@@ -1,33 +1,58 @@
 package distribution;
 
 import java.io.Serializable;
+import java.security.Key;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 @SuppressWarnings("serial")
 public class Encryption implements Serializable  {
 
 	public Encryption() {}
-
+	
+    private static final byte[] secretKey = new byte[]{'o', 'l', 'u', '4', 'a', 'p', 'o', 'a', 'o', 'j', '2'};
+    private static String transformation = "AES"; // AES/CBC/PKCS5Padding...
+    		
 	//Modelo de Cesar, onde troca uma letra por X letras seguintes
 	public Packet encrypt (Packet in) {
+		// first step
 		String message = in.getBody().getMessage().getBody().getContent();
-		String aux1="";
+		String aux = "";
 		for (int i = 0; i < message.length(); i++) {
-			if(message.charAt(i) != ' ') {
+			if (message.charAt(i) != ' ') {
 				char a = (char) (message.charAt(i)+3);
-				aux1 = aux1+a;
+				aux = aux+a;
 			} else {
-				aux1 = aux1+' ';
+				aux = aux+' ';
 			}
 		}
+		
+		//Second step
+//        Key key = generateKey();
+//        Cipher cipher = Cipher.getInstance(this.transformation);
+//        cipher.init(Cipher.ENCRYPT_MODE, key);
+//        byte[] value = cipher.doFinal(aux.getBytes());
+//        Base64.getEncoder().encodeToString(value);
+        
 		Packet retorno = in;
-		retorno.getBody().getMessage().getBody().setContent(aux1);
+		retorno.getBody().getMessage().getBody().setContent(aux);
 		return retorno;
 	}
 
 
 	public Packet decrypt(Packet in) {
 		String message = in.getBody().getMessage().getBody().getContent();
-		String aux1="";
+		
+//        Key key = generateKey();
+//        Cipher cipher = Cipher.getInstance(this.transformation);
+//        cipher.init(Cipher.DECRYPT_MODE, key);
+//        byte[] value = Base64.getDecoder().decode(message);
+//        byte[] valuePreCesar = cipher.doFinal(value);
+//        String aux = new String(valuePreCesar);
+		
+		String aux1 = "";
 		for (int i = 0; i < message.length(); i++) {
 			if(message.charAt(i) != ' ') {
 				char a = (char) (message.charAt(i)-3);
@@ -36,8 +61,14 @@ public class Encryption implements Serializable  {
 				aux1 = aux1+' ';
 			}
 		}
+
 		Packet retorno = in;
 		retorno.getBody().getMessage().getBody().setContent(aux1);
 		return retorno;
 	}
+	
+    private static Key generateKey() throws Exception {
+        return new SecretKeySpec(secretKey, transformation);
+    }
+	
 }
